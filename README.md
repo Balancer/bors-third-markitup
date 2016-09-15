ea575ef11490e697c134da57b9b0d29d727bd061
Balancer <balancer@balancer.ru>
2016-09-15 09:11:54 +0300

    Add code.

---
+++ README.md
@@ -0,0 +1,18 @@
+# markItUp! asset for BORS© framework
+
+## Install:
+
+    composer require fxp/composer-asset-plugin=*
+    composer require balancer/bors-third-markitup=*
+
+or
+
+composer.json:
+
+	"repositories": [
+		{ "type": "composer", "url": "https://asset-packagist.org" }
+	],
+
+and:
+
+    composer require balancer/bors-third-markitup=*
---
+++ src/markItUp.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace B2\jQuery;
+
+class markItUp
+{
+	static function appear($id, $settings = 'mySettings')
+	{
+		self::load();
+
+		jquery::on_ready("
+w=jQuery('$id').parents('td').width()
+m=jQuery('$id').markItUp($settings).css('height', function() {
+	var h = jQuery(this).css('line-height').match(/(\d+)(.*)/)
+	return (h[1]*jQuery(this).attr('rows'))+h[2]
+}).css('width', w)
+jQuery('.markItUp').css('width', w)
+");
+//	jQuery('#bbcode').height(300);
+	}
+
+	static function load()
+	{
+		template_jquery();
+		$base = config('jquery.markitup.base');
+		$set  = config('jquery.markitup.sets.bbcode');
+		$set_js  = config('jquery.markitup.set.js', "$set/set.js");
+
+		template_css("$base/skins/simple/style.css");
+		template_css("$set/style.css");
+
+		template_js_include("$base/jquery.markitup.js");
+		template_js_include($set_js);
+	}
+}
